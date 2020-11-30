6c57515b708eae9e1ee27cbf47e2034692b5029a
Clint Richards <richardsclint17@gmail.com>
2020-11-30 02:16:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,6 +56,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
+  input=input_to_index(input)
   if valid_move?(board, input)
     move(board, input, current_player(board))
     display_board(board)
